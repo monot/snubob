0a506f4c43962e8a0134c65869c662250c2f11a7
Joonhwan Lee <joonhwan@gmail.com>
2019-03-20 16:55:27 +0900

    Updated test code

--- api/api.py
+++ api/api.py
@@ -10,24 +10,17 @@ app = Flask(__name__)
 def hello():
     return 'snubob api server'
 
-
 @app.route('/api/telephone', methods=['POST'])
 def telephone():
-
     phone = {
         "감골식당":"02-880-5544",
-        "동원생활관식당":"02-880-8697",
-        "학생회관식당":"02-880-5543",
-        "예술계식당":"02-876-1006"
+        "동원관":"02-880-8697",
+        "학생회관":"02-880-5543",
+        "아름드리(예술)":"02-876-1006"
     }
     req = request.get_json()
     rest_name = req["action"]["detailParams"]["restaurant_name"]["value"]
-    # answer = ""
-    # if rest_name.encode('utf-8') == "감골식당":
-    #     answer = "1234 입니다."
-
     answer = "{}의 전화번호는 <br> {} 입니다.".format(rest_name.encode('utf-8'), phone[rest_name.encode('utf-8')])
-
     res = {
         "version": "2.0",
         "template": {
@@ -40,7 +33,10 @@ def telephone():
             ]
         }
     }
-
     return jsonify(res)
+
+@app.route('/api/menu', methods=['POST'])
+def menu():
+    
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
--- test codes/mini-crawl.py
+++ test codes/mini-crawl.py
@@ -2,31 +2,30 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 import urllib.request
 
+breakfast = {}
+lunch = {}
+dinner = {}
 with urllib.request.urlopen("http://mini.snu.kr/cafe/today/") as url:
     doc = url.read()
     soup = BeautifulSoup(doc, "html.parser")
     table = soup.find("table")
-    # print(table)
-
-    # results = {}
-    # for row in table.findAll('tr'):
-    #     aux = row.findAll('td')
-    #     results[aux[0].string] = aux[1].string
 
     trs = table.findAll('tr')
     flag = ""
-    menu = {}
+    
     for tr in trs:
         if tr.text in ["아침", "점심", "저녁"]:
             flag = tr.text
-            print(flag)
-            print("====")
+            # print(flag)
             continue
-        print(tr.select("td")[0].text)
-        print(str(tr.select("td")[1]).replace("<td class=\"menu\">", "").replace("</td>", "").replace("</span>", "</span> "))
-        # menu[tr.select("td")[0].text] = str(tr.select("td")[1]).replace("<td class=\"menu\">", "").replace("</td>", "")
-    
-    
-    # pprint(menu)
+        rest_key = tr.select("td")[0].text
+        rest_val = str(tr.select("td")[1]).replace("<td class=\"menu\">", "").replace("</td>", "").replace("</span>", "</span> ")
 
-    # menu["302동"]
+        if flag == "아침":
+            breakfast[rest_key] = rest_val
+        if flag == "점심":
+            lunch[rest_key] = rest_val
+        if flag == "저녁":
+            dinner[rest_key] = rest_val
+
+pprint(dinner)
